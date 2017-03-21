d3a9a536d57c75a9bcbf9319683134df726fd175
sammanthp007 <samman.thapa@bison.howard.edu>
2017-03-20 21:20:01 -0400

    set image class name as PFImageView and all other table stuffs

---
+++ instagram/HomeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeTableViewCell.swift
+//  instagram
+//
+//  Created by samman ios on 3/20/17.
+//  Copyright © 2017 samman ios. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- instagram/HomeViewController.swift
+++ instagram/HomeViewController.swift
@@ -7,19 +7,82 @@
 //
 
 import UIKit
+import Parse
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController,  UITableViewDelegate, UITableViewDataSource {
+     var posts = [PFObject]()
+    func getData(){
+        // construct PFQuery
+        let query = PFQuery(className: "Post")
+        query.order(byDescending: "createdAt")
+        query.includeKey("author")
+        query.limit = 20
+        
+        // fetch data asynchronously
+        query.findObjectsInBackground { (posts: [PFObject]?, error: Error?) -> Void in
+            if let posts = posts {
+                self.posts = posts
+                print("Post is : ", posts)
+                // do something with the data fetched
+            } else {
+                print("Error! : ", error?.localizedDescription)
+                // handle error
+            }
+            self.tableView.reloadData()
+        }
+    }
 
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        getData()
+        
+        
+    }
+    
+    
+    
     @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return (self.posts.count)
+        
+    }
+    
+    
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "photoCell", for: indexPath) as! HomeTableViewCell
+        let post = posts[indexPath.row]
+        let caption = post["caption"] as? String
+        let photoPFFile = post["media"] as? PFFile
+        print("PhotoPFFFile is : ", photoPFFile)
+        print("Till here working")
+        cell.workingImageView.file = photoPFFile
+        print("Not working after this")
+        cell.workingImageView.loadInBackground()
+        
+        cell.captionLabel.text = caption
+        return cell
+        
+        
     }
     
 
