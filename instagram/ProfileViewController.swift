3070c960bfdc53272abcd8cf4a6896978ca93def
sammanthp007 <samman.thapa@bison.howard.edu>
2017-03-20 21:12:27 -0400

    add logout in the profile view

---
+++ instagram/ProfileViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ProfileViewController.swift
+//  instagram
+//
+//  Created by samman ios on 3/20/17.
+//  Copyright © 2017 samman ios. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBOutlet weak var onLogOut: UIButton!
+
+    @IBAction func logOut(_ sender: Any) {
+        PFUser.logOut()
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "UserDidLogOut"), object: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
